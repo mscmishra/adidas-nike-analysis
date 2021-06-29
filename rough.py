b879b67daf0b7e35d1ba5ff86a2db2f3dcf8c99e
Chandra Deep Mishra <msc.mishra@gmail.com>
2021-06-29 17:52:40 +0530

    added a new file for test

---
+++ rough.py
@@ -0,0 +1,17 @@
+# Pages
+#     id
+#     url unique
+#     html
+#     error
+#     old rank
+#     new rank
+
+# Links
+#     from_id Unique
+#     to_id Unique
+
+# Webs
+#     url Unique
+
+text = None
+print(text)
